0e29a567d0afcde21d2bedd444a2249bd945ceac
TranLeOfficial <tranthanhnhank123@gmail.com>
2021-10-04 11:54:00 +0700

    Login with Google

--- MessengerV2/Resources/AppDelegate.swift
+++ MessengerV2/Resources/AppDelegate.swift
@@ -11,6 +11,7 @@
 import UIKit
 import FBSDKCoreKit
 import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,10 +23,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
+        //Facebook
         ApplicationDelegate.shared.application(
             application,
             didFinishLaunchingWithOptions: launchOptions
         )
+        
+        //Google
+        GIDSignIn.sharedInstance()?.clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance()?.delegate = self
 
         return true
     }
@@ -42,6 +48,63 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
             annotation: options[UIApplication.OpenURLOptionsKey.annotation]
         )
+        
+        //Login with Google Signin
+        return GIDSignIn.sharedInstance().handle(url)
     }
 }
     
+
+//MARK: - Login with Google
+extension AppDelegate: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        guard error == nil else {
+            if let err = error {
+                print("Failed to signin with Google ", err)
+            }
+            return
+        }
+        
+        guard let user = user else {
+            return
+        }
+        print("Did signin with Google: ", user)
+        
+        guard let email = user.profile.email,
+              let firstName = user.profile.givenName,
+              let lastName = user.profile.familyName else {
+            return
+        }
+        
+        DatabaseManager.shared.userExists(with: email, completion: { exists in
+            if !exists {
+                //insert to database
+                DatabaseManager.shared.insertUser(with: ChatAppUser(emailAddress: email,
+                                                                    firstName: firstName,
+                                                                    lastName: lastName,
+                                                                    password: ""))
+            }
+        })
+        
+        guard let authentication = user.authentication, let idToken = authentication.idToken else {
+            print("Missing Auth object off of google user")
+            return
+        }
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                       accessToken: authentication.accessToken)
+        
+        FirebaseAuth.Auth.auth().signIn(with: credential, completion: { authResult, error in
+            guard authResult != nil, error == nil else {
+                print("Failed to login with google credential _!")
+                return
+            }
+            print("Google login Succecssfull!")
+            NotificationCenter.default.post(name: .didLoginNotification, object: nil)
+        })
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        print("Google signin Disconnected!")
+    }
+}
+
